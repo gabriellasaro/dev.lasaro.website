c91f25e853a00b32c02d65eff3ed527ea62f7bbf
Gabriel Lasaro <gabriellasarosaleze@gmail.com>
2020-01-07 22:20:44 -0300

    Substituiçao do jqueryy

--- js/coins.js
+++ js/coins.js
@@ -1,19 +1,30 @@
 function get_coin(coin, symbol){
-    $.ajax({
-        url: 'https://min-api.cryptocompare.com/data/top/exchanges/full?fsym='+coin.toUpperCase()+'&tsym='+symbol,
-        type: 'get',
-        dataType: 'json',
-    }).done(function(data){
-        var percent = ((data['Data']['AggregatedData']['PRICE']*100)/data['Data']['AggregatedData']['OPEN24HOUR']-100).toFixed(2).toString();
-        if (percent>=0){
-            $('.coin.'+coin+' #master').text("⬆️ R$"+data['Data']['AggregatedData']['PRICE'].toString())
-            $('.coin.'+coin+' #a').text("+"+percent+"%")
-        }else{
-            $('.coin.'+coin+' #master').text("⬇️ R$"+data['Data']['AggregatedData']['PRICE'].toString())
-            $('.coin.'+coin+' #a').text(percent+"%")
+    fetch('https://min-api.cryptocompare.com/data/top/exchanges/full?fsym='+coin.toUpperCase()+'&tsym='+symbol).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        let percent = ((data['Data']['AggregatedData']['PRICE']*100)/data['Data']['AggregatedData']['OPEN24HOUR']-100).toFixed(2);
 
+        let root = document.querySelector('.coin.'+coin+' .price');
+        let ele_price = document.createElement('span');
+        let ele_percent = document.createElement('span');
+        ele_percent.classList.add('txt');
+
+        if (percent>=0){
+            ele_price.textContent = '⬆️ R$'+data['Data']['AggregatedData']['PRICE'].toString().replace('.', ',');
+            ele_percent.style.color = '#3689e6';
+            ele_percent.textContent = ' +'+percent.toString().replace('.', ',')+'%';
+        }else{
+            ele_price.textContent = '⬇️ R$'+data['Data']['AggregatedData']['PRICE'].toString().replace('.', ',');
+            ele_percent.style.color = '#ed5353';
+            ele_percent.textContent = ' '+percent.toString().replace('.', ',')+'%';
         }
+        root.textContent = '';
+        root.appendChild(ele_price);
+        root.appendChild(ele_percent);
+    }).catch(function() {
+        console.log("Error!");
     });
 }
+
 get_coin('btc', 'BRL');
 get_coin('eth', 'BRL');
